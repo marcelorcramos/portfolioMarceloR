24a22d1c39417c33681993534910ed4d3bef5d81
Marcelo Ramos <marcelorcramos@gmail.com>
2025-02-28 17:39:41 +0000

    App Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import RoundOne from "./components/roundDesigns/RoundOne";
 import RoundTwo from "./components/roundDesigns/RoundTwo";
 import RoundThree from "./components/roundDesigns/RoundThree";
@@ -8,18 +9,22 @@ import Home from "./Home";
 
 function App() {
   return (
-    <div className="w-full lgl:h-screen font-bodyfont overflow-hidden text-textColor bg-black relative">
-      <div className="max-w-screen-2xl h-full mx-auto flex justify-center items-center">
-        <Home />
+    <Router>
+      <div className="w-full lgl:h-screen font-bodyfont overflow-hidden text-textColor bg-black relative">
+        <div className="max-w-screen-2xl h-full mx-auto flex justify-center items-center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <div className="w-full h-full absolute top-0 left-0 z-10">
+          <RoundOne />
+          <RoundTwo />
+          <RoundThree />
+          <RoundFour />
+          <RoundFive />
+        </div>
       </div>
-      <div className="w-full h-full absolute top-0 left-0 z-10">
-        <RoundOne />
-        <RoundTwo />
-        <RoundThree />
-        <RoundFour />
-        <RoundFive />
-      </div>
-    </div>
+    </Router>
   );
 }
 
