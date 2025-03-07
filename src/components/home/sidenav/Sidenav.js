644acad336e646f7bb0b8f55406a292aca188102
Marcelo Ramos <marcelorcramos@gmail.com>
2025-03-07 21:50:12 +0000

    Update sideNav.js

--- src/components/home/sidenav/Sidenav.js
+++ src/components/home/sidenav/Sidenav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SidenavTitle from './SidenavTitle';
+import { Link } from "react-router-dom";
 
 const Sidenav = () => {
   return (
@@ -8,14 +9,38 @@ const Sidenav = () => {
       <ul>
         <li className="sidenavLi">Portfolio Page</li>
       </ul>
-      <SidenavTitle title="P" subTitle="rojects" />
+        <SidenavTitle title="P" subTitle="rojects" />
       <ul>
-        <li className="sidenavLi">VEX APP</li>
-        <li className="sidenavLi">Weather App</li>
-        <li className="sidenavLi">Contact List</li>
-        <li className="sidenavLi">Github Scraper</li>
-        <li className="sidenavLi">Finance Control</li>
-        <li className="sidenavLi">Projeto Centro De Saúde</li>
+      <li className="sidenavLi">
+      <a href="https://github.com/marcelorcramos/VexApp" target="_blank" rel="noopener noreferrer">
+        VEX APP
+      </a>
+      </li>
+      <li className="sidenavLi">
+      <a href="https://github.com/marcelorcramos/VexApp" target="_blank" rel="noopener noreferrer">
+        Weather APP
+      </a>
+      </li>
+      <li className="sidenavLi">
+      <a href="https://github.com/marcelorcramos/VexApp" target="_blank" rel="noopener noreferrer">
+        Finance Control
+      </a>
+      </li>
+      <li className="sidenavLi">
+      <a href="https://github.com/marcelorcramos/VexApp" target="_blank" rel="noopener noreferrer">
+        VEX APP
+      </a>
+      </li>
+      <li className="sidenavLi">
+      <a href="https://github.com/marcelorcramos/VexApp" target="_blank" rel="noopener noreferrer">
+        VEX APP
+      </a>
+      </li>
+      <li className="sidenavLi">
+      <a href="https://github.com/marcelorcramos/VexApp" target="_blank" rel="noopener noreferrer">
+        VEX APP
+      </a>
+      </li>
       </ul>
       <SidenavTitle title="R" subTitle="each Me" />
       <ul>
